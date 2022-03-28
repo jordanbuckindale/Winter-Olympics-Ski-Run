5c1b3b264087ddd5ae5f344570801d29f87c1f3b
jbuckind <jbuckind@uwo.ca>
2022-03-28 15:08:41 -0400

    ...

---
+++ src/Ski.java
@@ -0,0 +1,24 @@
+
+public class Ski {
+
+	/**
+	 *  declares root node of tree.
+	 */
+	private BinaryTreeNode<SkiSegment> root;
+	
+	
+	// constructor
+	public Ski(String[] data) {
+		
+	}
+	
+	// method
+	public BinaryTreeNode<SkiSegment> getRoot() {
+		return null;
+	}
+	
+	// method
+	public void skiNextSegment (BinaryTreeNode<SkiSegment> node, ArrayUnorderedList<SkiSegment> sequence) {
+		
+	}
+}
---
+++ src/TreeBuilder.java
@@ -0,0 +1,9 @@
+
+public class TreeBuilder {
+
+	
+	public <T> LinkedBinaryTree<T> buildTree(T[] data) {
+		return null;
+		
+	}
+}
